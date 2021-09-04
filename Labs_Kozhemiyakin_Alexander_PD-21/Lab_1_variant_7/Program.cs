761458bdc875ac8227b2b74a74bed234b9cb94b4
abeba <sashok.supero@gmail.com>
2021-09-04 18:55:35 +0300

    has done lab #1 and #2

---
+++ Labs_Kozhemiyakin_Alexander_PD-21/Lab_1_variant_7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1_variant_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            double h;
+
+            Console.Write("Введите радиус конуса: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите высоту конуса: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            double v = Math.Round(((Math.PI * Math.Pow(r, 2) * h) / 3), 2);
+
+            Console.WriteLine($"Объём конуса = {v}");
+        }
+    }
+}
---
+++ Labs_Kozhemiyakin_Alexander_PD-21/Lab_2_variant_7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_variant_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.Write("Введіть n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть k: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сума чи добуток?  --> ");
+            string choice = Console.ReadLine();
+            double res = choice.ToLower() == "сума" ? 0 : 1;
+
+            for(int i = n*n; i<= n * k; i++)
+            {
+                if(choice.ToLower() == "сума")
+                {
+                    res += ((k*k + Math.Pow(-1, k-1) * (2*k) - 1) / (k*k + 8));
+                }
+                else
+                {
+                    res *= ((k * k + Math.Pow(-1, k - 1) * (2 * k) - 1) / (k * k + 8));
+                }
+            }
+
+            Console.WriteLine($"Результат: {Math.Round(res, 2)}");
+        }
+    }
+}
