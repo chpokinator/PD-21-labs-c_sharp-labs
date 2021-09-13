38fc26f0ca9dc79791cd9405c439384b2fa55320
Aboba <sashok.supero@gmail.com>
2021-09-13 15:02:31 +0300

    labs 3 and 4 have been done

--- Labs_Kozhemiyakin_Alexander_PD-21/Lab_3_variant_7/Program.cs
+++ Labs_Kozhemiyakin_Alexander_PD-21/Lab_3_variant_7/Program.cs
@@ -10,6 +10,23 @@ namespace Lab_3_variant_7
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.Write("Введіть число 1: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть число 2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введіть число 3: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if(a == b || a==c || b == c)
+            {
+                Console.WriteLine("Серед чисел є пара співпадаючих");
+            }
+            else
+            {
+                Console.WriteLine("Серед чисел намає співпадаючих пар");
+            }
         }
     }
 }
--- Labs_Kozhemiyakin_Alexander_PD-21/Lab_4_variant_7/Program.cs
+++ Labs_Kozhemiyakin_Alexander_PD-21/Lab_4_variant_7/Program.cs
@@ -10,6 +10,38 @@ namespace Lab_4_variant_7
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+
+
+            Console.Write("Введіть розмір масиву: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            int[] arr = new int[n];
+            Console.Write("Початковий масив: ");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(-100, 100);
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n");
+            int min = arr.Min();
+            int max = arr.Max();
+            Console.Write("Вихідний масив: ");
+            for(int i = 0; i<arr.Length; i++)
+            {
+                if(arr[i] > 0)
+                {
+                    arr[i] = min;
+                }
+                else if (arr[i] < 0)
+                {
+                    arr[i] = max;
+                }
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
